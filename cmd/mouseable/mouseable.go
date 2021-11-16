1731fafbb84a7bef33e2ec4c3f8a0791e3b7d787
wirekang <wirekang@gmail.com>
2021-11-16 23:10:42 +0900

    Fix flags

--- cmd/mouseable/mouseable.go
+++ cmd/mouseable/mouseable.go
@@ -2,21 +2,60 @@ package main
 
 import (
 	"flag"
+	"os"
 
 	"github.com/wirekang/mouseable/internal/check"
 	"github.com/wirekang/mouseable/internal/config"
+	"github.com/wirekang/mouseable/internal/lg"
+	"github.com/wirekang/mouseable/internal/svc"
 )
 
 func main() {
 	check.MustWindows()
 
-	configPath := flag.String(
-		"config", config.DefaultConfigPath, "config file path",
+	register := flag.Bool("register", false, "Register and run service")
+	unregister := flag.Bool("unregister", false, "Unregister service")
+	reload := flag.Bool(
+		"reload", false, "Reload config file at  "+config.ConfigPath,
+	)
+	run := flag.Bool(
+		"run", false,
+		"This flag run mouseable in foreground that usually NOT NEEDED",
 	)
 	flag.Parse()
 
-	_, err := config.Load(*configPath)
-	if err != nil {
-		panic(err)
+	if *register {
+		err := svc.Register()
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	if *unregister {
+		err := svc.Unregister()
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	if *reload {
+		err := svc.Reload()
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	if !(*run || *register || *unregister || *reload) {
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+
+	if *run {
+		lg.Logf("start")
+		_, err := config.Load()
+		if err != nil {
+			lg.Errorf("config.Load: %v", err)
+			os.Exit(1)
+		}
 	}
 }
