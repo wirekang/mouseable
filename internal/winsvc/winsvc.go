bee073554aa32d1f63b4d83b2761ff0b6687a1e3
wirekang <wirekang@gmail.com>
2021-11-17 03:03:24 +0900

    Remove test logging

--- internal/winsvc/winsvc.go
+++ internal/winsvc/winsvc.go
@@ -2,15 +2,11 @@ package winsvc
 
 import (
 	"context"
-	"os"
-	"strconv"
 	"time"
 
 	"golang.org/x/sys/windows/svc"
-	"golang.org/x/sys/windows/svc/eventlog"
 
 	"github.com/wirekang/mouseable/internal/lg"
-	"github.com/wirekang/mouseable/internal/script"
 )
 
 type Handler struct {
@@ -22,21 +18,6 @@ func (h Handler) Execute(
 	requestChan <-chan svc.ChangeRequest,
 	statusChan chan<- svc.Status,
 ) (svcSpecificEC bool, exitCode uint32) {
-	elog, _ := eventlog.Open(script.ServiceName)
-	f, err := os.Create(lg.FilePath)
-	elog.Info(1, lg.FilePath)
-	if err != nil {
-		elog.Info(2, err.Error())
-	} else {
-		n, err := f.WriteString("asdf")
-		if err != nil {
-			elog.Info(3, err.Error())
-		} else {
-			elog.Info(4, strconv.Itoa(n))
-
-		}
-	}
-
 	statusChan <- svc.Status{State: svc.StartPending}
 	ctx, cancel := context.WithCancel(context.Background())
 	go h.RunFunc(ctx)
