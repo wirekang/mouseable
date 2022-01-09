9f5dc55b42cec04d50816162fa2791ba93f04a4f
wirekang <wirekang@gmail.com>
2022-01-09 20:46:59 +0900

    Clean for monaco

--- ts/src/App.tsx
+++ ts/src/App.tsx
@@ -1,23 +1,12 @@
 import React, { useState } from "react";
-import { changeFunction, FunctionKey, loadBind } from "./gobind";
+import { loadBind } from "./gobind";
 import { useAsync } from "react-use";
-import FunctionGroupBox from "./components/FunctionGroupBox";
-import FunctionKeyInput from "./components/FunctionKeyInput";
-import Background from "./components/Background";
-import MyContext from "./MyContext";
-import DataGroupBox from "./components/DataGroupBox";
-import InfoGroupBox from "./components/InfoGroupBox";
+import GitHubButton from "./components/GitHubButton";
+import TerminateButton from "./components/TerminateButton";
 
 function App() {
   const [requesterCounter, setRequesterCounter] = useState(0);
   const goBindState = useAsync(loadBind, [requesterCounter]);
-  const [keyInputState, setKeyInputState] = useState<{
-    isOpen: boolean;
-    name?: string;
-    key?: FunctionKey;
-  }>({
-    isOpen: false,
-  });
 
   if (goBindState.loading || !goBindState.value) {
     return <h4>Loading...</h4>;
@@ -25,45 +14,22 @@ function App() {
 
   const goBind = goBindState.value!;
 
-  const requestChangeFunctionKey = (name: string) => {
-    const key = goBind.functionNameKeyMap[name];
-    setKeyInputState({ name, isOpen: true, key });
-  };
-
-  const closeFunctionKeyInput = () => {
-    setKeyInputState({ isOpen: false, key: undefined, name: undefined });
-  };
-
-  const changeFunctionKeyInput = async (name: string, key: FunctionKey) => {
-    const rst = await changeFunction(name, key);
-    if (rst) {
-      setKeyInputState({ isOpen: false });
-      setRequesterCounter((v) => v + 1);
-    }
-  };
-
   return (
-    <MyContext.Provider
-      value={{
-        requestChangeFunctionKey,
-      }}
-    >
-      <div>
-        <InfoGroupBox version={goBind.version} />
-        <FunctionGroupBox def={goBind.functionDefinitions} record={goBind.functionNameKeyMap} />
-        <DataGroupBox def={goBind.dataDefinitions} record={goBind.dataNameValueMap} />
-
-        {keyInputState.isOpen && keyInputState.name && keyInputState.key && (
-          <Background onClick={closeFunctionKeyInput}>
-            <FunctionKeyInput
-              name={keyInputState.name}
-              fKey={keyInputState.key}
-              change={changeFunctionKeyInput}
-            />
-          </Background>
-        )}
+    <div>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          alignItems: "center",
+          justifyContent: "space-between",
+          margin: 3,
+        }}
+      >
+        <span>Version: {goBind.version}</span>
+        <GitHubButton />
+        <TerminateButton />
       </div>
-    </MyContext.Provider>
+    </div>
   );
 }
 
